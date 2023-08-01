d558d5c02945d51d176cc62f405e71671d8efe2d
begindeveloper <107010469+begindeveloper@users.noreply.github.com>
2023-07-31 22:17:47 -0500

    update 06-Aug-2023

---
+++ script.js
@@ -0,0 +1,16 @@
+const gameboard = document.querySelector('#gameboard')
+const infoDisplay= document.querySelector('#info')
+const startCells = [
+    "", "", "", "", "", "", "", "", ""
+]
+
+
+function createBoard(){
+    startCells.forEach((cell, index) => {
+        const cellElement = document.createElement('div');
+        cellElement.classList.add('square')
+        gameboard.append(cellElement)
+    })
+}
+
+createBoard()
