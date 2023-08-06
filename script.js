eccbd4afdc992c8836e65cd1f4394e0aff8e9dcc
begindeveloper <107010469+begindeveloper@users.noreply.github.com>
2023-08-06 15:14:50 -0500

    update 06-08-2023

--- script.js
+++ script.js
@@ -5,12 +5,19 @@ const startCells = [
 ]
 
 
-function createBoard(){
-    startCells.forEach((cell, index) => {
+function createBoard() {
+    startCells.forEach((_cell, index) => {
         const cellElement = document.createElement('div');
         cellElement.classList.add('square')
+        cellElment.id = index
+        cellElement.addEventListener('click', addGo)
         gameboard.append(cellElement)
     })
 }
 
 createBoard()
+
+
+function addGo(e) {
+    
+}
