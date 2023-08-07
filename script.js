9c6550f20757e4c5b507e94259678a78348beda4
begindeveloper <107010469+begindeveloper@users.noreply.github.com>
2023-08-06 22:52:13 -0500

    update 06-08-2023

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const startCells = [
     "", "", "", "", "", "", "", "", ""
 ]
 
+let go = "circle"
+infoDisplay.textContent = "Circle goes first"
 
 function createBoard() {
     startCells.forEach((_cell, index) => {
         const cellElement = document.createElement('div');
         cellElement.classList.add('square')
-        cellElment.id = index
+        cellElement.id = index
         cellElement.addEventListener('click', addGo)
         gameboard.append(cellElement)
     })
@@ -19,5 +21,41 @@ createBoard()
 
 
 function addGo(e) {
-    
+    const goDisplay = document.createElement('div')
+    goDisplay.classList.add(go)
+    e.target.append(goDisplay)
+    go = go === "circle" ? "cross" : "circle"
+    infoDisplay.textContent = "It is now " + go + "'s go."
+    e.target.removeEventListner('click', addGo)
+    checkScore()
+}
+
+function checkScore() {
+    const allSquares = document.querySelectorAll('.square')
+    const winningCombos = [
+        ["0","1","2"],["3","4","5"],["6","7","8"]
+        ,["0","3","6"],["1","4","7"],["2","5","8"]
+        ,["0","4","8"],["2","4","6"]
+    ]
+
+    winningCombos.forEach(array => {
+        const circleWins = array.every(cell => 
+            allSquares[cell].firstChild.classList.contains('circle'))
+            if (circleWins){
+                alert(`You won! ${go} wins`)
+                allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+                return
+                }
+    })
+
+
+    winningCombos.forEach(array => {
+        const crossWins = array.every(cell => 
+            allSquares[cell].firstChild.classList.contains('cross'))
+            if (crossWins){
+                alert(`You won! ${go} wins`)
+                allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+                return
+                }
+    })
 }
