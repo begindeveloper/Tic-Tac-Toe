59570c7369fd3b906d85cf89334b47d9b5054200
begindeveloper <107010469+begindeveloper@users.noreply.github.com>
2023-08-21 15:34:42 -0500

    update 08-21-2023

--- script.js
+++ script.js
@@ -1,74 +1,65 @@
-const gameboard = document.querySelector('#gameboard')
-const infoDisplay= document.querySelector('#info')
-const startCells = ["", "", "", "", "", "", "", "", ""];
+const gameboard = document.querySelector('#gameboard');
+const infoDisplay = document.querySelector('#info');
+const resetButton = document.querySelector('button');
 
-let go = "circle";
+let currentPlayer = 'circle';
 infoDisplay.textContent = "Circle goes first";
+
+const startCells = ["", "", "", "", "", "", "", "", ""];
 
 function createBoard() {
     startCells.forEach((_cell, index) => {
         const cellElement = document.createElement('div');
-        cellElement.classList.add('square')
+        cellElement.classList.add('square');
         cellElement.id = index;
         cellElement.addEventListener('click', addGo);
-        gameboard.append(cellElement);
+        gameboard.appendChild(cellElement);
     });
 }
 
 createBoard();
 
-
 function addGo(e) {
-    const goDisplay = document.createElement('div');
-    goDisplay.classList.add(go);
-    e.target.append(goDisplay);
-    go = go === "circle" ? "cross" : "circle";
-    infoDisplay.textContent = "It is now " + go + "'s go.";
-    e.target.removeEventListener('click', addGo);
-    checkScore()
-}
+    const cell = e.target;
 
+    if (cell.classList.contains('square') && cell.textContent === "") {
+        cell.textContent = currentPlayer;
+        currentPlayer = currentPlayer === "circle" ? "cross" : "circle";
+        infoDisplay.textContent = "It is now " + currentPlayer + "'s turn.";
+        cell.removeEventListener('click', addGo);
+        checkScore();
+    }
+}
 
 function checkScore() {
-    const allSquares = document.querySelectorAll('.square');
+    const squares = document.querySelectorAll('.square');
     const winningCombos = [
-        ["0","1","2"],["3","4","5"],["6","7","8"]
-        ,["0","3","6"],["1","4","7"],["2","5","8"]
-        ,["0","4","8"],["2","4","6"]
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
     ];
 
-    winningCombos.forEach(array => {
-        const circleWins = array.every(cell => 
-            allSquares[cell].firstChild?.classList.contains('circle'))
-
-        if (circleWins){
-            infoDisplay.textContent = "Circle Wins";
-            allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
-            return
+    for (const combo of winningCombos) {
+        const [a, b, c] = combo;
+        if (squares[a].textContent && squares[a].textContent === squares[b].textContent && squares[a].textContent === squares[c].textContent) {
+            infoDisplay.textContent = currentPlayer === "circle" ? "Circle Wins!" : "Cross Wins!";
+            return;
         }
-        resetBoard();
-     });
+    }
 
-
-     winningCombos.forEach(array => {
-        const crossWins = array.every(cell => 
-            allSquares[cell].firstChild?.classList.contains('cross'))
-
-        if (crossWins){
-            infoDisplay.textContent = "Cross Wins!";
-            allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
-            return
-         }
-         resetBoard();
-     });
+    if ([...squares].every(square => square.textContent !== "")) {
+        infoDisplay.textContent = "It's a Draw!";
+    }
 }
 
-function resetBoard() {
-    const allSquares = document.querySelectorAll('.square');
-    allSquares.forEach(square => {
-      if (square.firstChild) {
-        square.removeChild(square.firstChild);
-      }
-      square.addEventListener('click', addGo);
+function resetGame() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.textContent = "";
+        square.addEventListener('click', addGo);
     });
-  }
+    currentPlayer = 'circle';
+    infoDisplay.textContent = "Circle goes first";
+}
+
+resetButton.addEventListener('click', resetGame);
